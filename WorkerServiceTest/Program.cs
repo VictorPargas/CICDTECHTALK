3e274a72eec0adbe9b52ea82b07c8ccf609cc647
VictorPargas <victorparga765@gmail.com>
2025-12-09 16:23:20 -0300

    ajustes

--- WorkerServiceTest/Program.cs
+++ WorkerServiceTest/Program.cs
@@ -1,7 +1,17 @@
 using WorkerServiceTest;
 
-var builder = Host.CreateApplicationBuilder(args);
+var builder = Host.CreateApplicationBuilder(new HostApplicationBuilderSettings
+{
+    ContentRootPath = AppContext.BaseDirectory
+});
+
 builder.Services.AddHostedService<Worker>();
+
+
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "Worker Service Test";
+});
 
 var host = builder.Build();
 host.Run();
