c91ae6bce23d27a49a8bdc368b0dd7835544aba3
VictorPargas <victorparga765@gmail.com>
2025-12-11 23:24:50 -0300

    feat: teste ao vivo

--- WorkerServiceTest/Worker.cs
+++ WorkerServiceTest/Worker.cs
@@ -15,7 +15,7 @@ namespace WorkerServiceTest
             {
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
-                    _logger.LogInformation("Worker running atf: {time}", DateTimeOffset.Now);
+                    _logger.LogInformation("Worker rodando agora : {time}", DateTimeOffset.Now);
                 }
                 await Task.Delay(1000, stoppingToken);
             }
